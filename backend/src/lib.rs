f09cfb1485f0eabdb39b80de82bf8529b30085eb
Suyashtnt <suyashtnt@gmail.com>
2024-06-27 11:56:20 +0200

    feat: database shenanigians

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,5 @@
-use std::{fmt, future::Future};
+use error_stack::{Result, ResultExt};
+use std::{error::Error, fmt, future::Future};
 
 pub mod auth;
 pub mod chess;
@@ -12,8 +13,35 @@ impl fmt::Display for ServiceError {
     }
 }
 
+impl Error for ServiceError {}
+
 pub trait Service {
+    /// The name of the service used in logs
     const SERVICE_NAME: &'static str;
 
+    /// Run the service
+    ///
+    /// # Returns
+    /// Returns OK(()) If it runs successfully and it was intended to end
+    /// Else returns an error if it fails (and consequently takes down the whole app)
     fn run(self) -> impl Future<Output = Result<(), ServiceError>> + Send + Sync;
+}
+
+#[derive(Debug, Clone)]
+pub struct BackendService {
+    pg_pool: sqlx::postgres::PgPool,
+}
+
+impl Service for BackendService {
+    const SERVICE_NAME: &'static str = "Backend Scripts";
+
+    /// Initializes the database and fills up various caches
+    async fn run(self) -> Result<(), ServiceError> {
+        sqlx::migrate!()
+            .run(&self.pg_pool)
+            .await
+            .change_context(ServiceError)?;
+
+        Ok(())
+    }
 }
